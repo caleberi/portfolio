8121a94cce15db838c0679c7f17d1ad367f66f9a
DevYoma <Lawrenceyoma@gmail.com>
2022-05-30 16:24:15 +0100

    done with integrating API

--- src/Pages/QuizPage.tsx
+++ src/Pages/QuizPage.tsx
@@ -1,23 +1,47 @@
 import React, { useState } from 'react'
 import QuestionCard from '../Components/QuestionCard'
-import { fetchQuizQuestions, Difficulty } from '../utils/Api'
+import { fetchQuizQuestions, Difficulty, QuestionState } from '../utils/Api'
 
 const TOTAL_QUESTIONS = 10
+
+type AnswerObject = {
+    question: string;
+    answer: string;
+    correct: boolean;
+    correctAnswer: string;
+}
 
 export const QuizPage = () => {
 
     const [loading, setLoading] = useState(false)
-    const [questions, setQuestions] = useState([])
+    const [questions, setQuestions] = useState<QuestionState[]>([])
     const [number, setNumber] = useState(0)
-    const [userAnswers, setUserAnswers] = useState([])
+    const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
     const [score, setScore] = useState(0)
     const [gameOver, setGameOver] = useState(true)
 
-    console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+    // console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+    console.log(questions)
 
     // starting game
     const startTrivia = async () => {
+        // clicking the start button
+        setLoading(true)
+        setGameOver(false)
 
+        const newQuestions = await fetchQuizQuestions(
+            TOTAL_QUESTIONS,
+            Difficulty.EASY
+        )
+
+        setQuestions(newQuestions)
+
+        setScore(0)
+        setUserAnswers([])
+        setNumber(0)
+
+        setLoading(false)
+        
     }
 
     // selectinng answer
@@ -33,8 +57,13 @@ export const QuizPage = () => {
     return(
         <div>
             <h2>REACT TYPESCRIPT QUIZ</h2>
-            <button className='question__start' onClick={startTrivia}>Start</button>
-            <p className="question__score">Score:</p>
+            {
+                gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+                    <button className='question__start' onClick={startTrivia}>Start</button>
+                ) : null
+            }
+            
+            {!gameOver ? <p className="question__score">Score:</p> : null}
             <p>Loading Questions</p>
 
             {/* <QuestionCard 
--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,19 +1,45 @@
+import { shuffleArray } from "./QuestionUtils";
+
 // enum
 export enum Difficulty {
     EASY = "easy",
-    MEDUIM = "medium",
+    MEDIUM = "medium",
     HARD = "hard"
 }
+
+// props from the API
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string
+}
+
+// this uses types from the Question and adds answers property to it.
+export type QuestionState = Question & { answers: string[] }
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
 
     // cool, double await 😲
-    // const data = await(await fetch(endpoint)).json()
+    const data = await(await fetch(endpoint)).json()
 
-    const fetchedData = await fetch(endpoint)
+    // const fetchedData = await fetch(endpoint)
 
-    const data = await fetchedData.json()
+    // const data = await fetchedData.json()
 
-    console.log(data)
+    // console.log(data)
+
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answers: shuffleArray([
+                ...question.incorrect_answers,
+                question.correct_answer
+            ])
+        }
+    ))
+    
 }
