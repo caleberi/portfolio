fdcfce836d2ed1aa53778d98dcc438a1233718d5
Devyoma <lawrenceyoma@gmail.com>
2022-06-03 09:42:55 +0100

    victors music player

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   Route,
 } from "react-router-dom";
 import { QuizPage } from './Pages/QuizPage'
+import VictorPage from './Pages/VictorPage';
 
 const App = () => {
   return(
@@ -14,6 +15,7 @@ const App = () => {
 
         <Route path="/" element={<QuizPage />}/>
 
+        <Route path="/victor" element={<VictorPage />} />
       </Routes>
     </BrowserRouter>
   )
