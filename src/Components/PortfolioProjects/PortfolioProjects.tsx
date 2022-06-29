5a1c9016516ef3f76b7e7d4ac8d1f7022f5cb615
caleberi <caleberioluwa@gmail.com>
2022-06-29 16:56:12 +0100

    feat  : add new project update

--- src/Components/PortfolioProjects/PortfolioProjects.tsx
+++ src/Components/PortfolioProjects/PortfolioProjects.tsx
@@ -1,17 +1,15 @@
 import React, { useState } from 'react'
 import './PortfolioProjects.css';
 import Pic1 from '../../assets/portfolioAssets/1-1.jpg'
-import Pic2 from '../../assets/portfolioAssets/2-1.jpg'
-import Pic3 from '../../assets/portfolioAssets/3-1.jpg'
-import Pic4 from '../../assets/portfolioAssets/4-1.jpg'
-import Pic5 from '../../assets/portfolioAssets/5-1.jpg'
-import Pic6 from '../../assets/portfolioAssets/6-1.jpg'
+import Pic2 from '../../assets/portfolioAssets/natour.png'
+import Pic3 from '../../assets/portfolioAssets/pm2.png'
 
 type CardProp = {
   id: number;
   image: string;
   name: string;
   detail: string;
+  link:string
 }
 
 const PortfolioProjects = () => {
@@ -19,86 +17,29 @@ const PortfolioProjects = () => {
     {
       id: 1,
       image: Pic3,
-      name: 'SoundCloud Audio',
-      detail: 'Audio SoundCloud'
+      name: 'Process Monitor',
+      detail: 'The process monitor showing you what’s happening on your computer: processes, id, CPU and memory usage, and much more.',
+      link:"https://github.com/caleberi/process-monitor"
     },
     {
       id: 2,
       image: Pic1,
-      name: 'Detailed Project 2',
-      detail: 'Project 2'
+      name: 'Express-gallery-service',
+      detail: 'A minimal dynamically rendered website using EJS ,Node,& Express for Artist and their works . The application gathers feedback about each individual artist on the site.  ',
+      link:"https://dot-treasure-constellation.glitch.me/"
     },
     {
       id: 3,
-      image: Pic2,
-      name: 'Vimeo Video',
-      detail: 'Vimeo Video 1'
-    },
-    {
-      id: 1,
-      image: Pic4,
-      name: 'Detailed Project 1',
-      detail: 'Project 1'
-    },
-    {
-      id: 1,
-      image: Pic5,
-      name: 'Mockup Design 1',
-      detail: 'Mockups'
-    },
-    {
-      id: 1,
-      image: Pic6,
-      name: 'SoundCloud Audio',
-      detail: 'Audio SoundCloud'
-    },
-  ])
-
-  const [projectLinks] = useState([
-    {
-      id: 1,
-      linkName: 'All'
-    }, 
-    {
-      id: 2, 
-      linkName: 'Audio'
-    },
-    {
-      id: 3, 
-      linkName: 'Detailed'
-    },
-    {
-      id: 4, 
-      linkName: 'MockUps'
-    },
-    {
-      id: 5, 
-      linkName: 'SoundCloud'
-    },
-    {
-      id: 6, 
-      linkName: 'Video'
-    },
-    {
-      id: 7, 
-      linkName: 'Vimeo'
-    },
-    {
-      id: 8, 
-      linkName: 'Youtube'
+      image:Pic2,
+      name: 'Natour',
+      detail: 'A tour booking application using Node JS , Redis, & other technologies',
+      link:"https://github.com/caleberi/Natour"
     },
   ])
 
   return (
         <div className="portfolioProjects">
           <h2 className="portfolioProjects__header">Portfolio</h2>
-
-          <div className="portfolioProjects__links">
-            {projectLinks.map(projectLink => (
-              <li>{projectLink.linkName}</li>
-            ))}
-          </div>
-
           <div className="portfolioProjects__items">
             {projects.map(project => (
               <div className="portfolioProjects__item" key={project.id}>
@@ -106,6 +47,8 @@ const PortfolioProjects = () => {
                   <img src={project.image} alt={`project${project.id}`} />
                 </div>
                 <p>{project.name}</p>
+                <p className='portfolioProjects__Paragraph'>{project.detail}</p>
+                <a className="portfolioProjects__Link" href={project.link}>Click Here</a>
               </div>
             ))}
           </div>
