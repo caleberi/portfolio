a3e2039afcda865d579631ccbc06294567f1984a
caleberi <caleberioluwa@gmail.com>
2022-06-29 16:56:53 +0100

    refact :  code tweak to personal

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-import { QuizPage } from './Pages/QuizPage'
 import PortfolioPage from './Pages/PortfolioPage/PortfolioPage';
 
 const App = () => {
   return(
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<PortfolioPage />} />
-
-        <Route path="/quiz" element={<QuizPage />}/>
-
       </Routes>
     </BrowserRouter>
   )
