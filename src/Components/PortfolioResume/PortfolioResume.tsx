54aae498fdf6a3160a82b767a873d0346a5704e9
caleberi <caleberioluwa@gmail.com>
2022-06-29 16:54:24 +0100

    feat : add new project page update

--- src/Components/PortfolioResume/PortfolioResume.tsx
+++ src/Components/PortfolioResume/PortfolioResume.tsx
@@ -1,10 +1,6 @@
 import React, { useState } from 'react'
-import ResumeCertificates from '../../MiniComponents/ResumeCertificates/ResumeCertificates'
-import ResumeKnowledgeButton from '../../MiniComponents/ResumeKnowledgeButton/ResumeKnowledgeButton'
-import ResumeProgressBar from '../../MiniComponents/ResumeProgressBar/ResumeProgressBar'
 import './PortfolioResume.css'
-import Client2 from '../../assets/portfolioAssets/client-2.png';
-import Client6 from '../../assets/portfolioAssets/client-6.png';
+import Client2 from '../../assets/portfolioAssets/certificate-1.png';
 import ResumeEducation from '../../MiniComponents/ResumeEducation/ResumeEducation'
 import ResumeExperience from '../../MiniComponents/ResumeExperience/ResumeExperience'
 
@@ -14,82 +10,67 @@ const PortfolioResume = () => {
     {
       id: 1, 
       image: Client2,
-      title: 'Psychology of intertnation Design',
-      membershipId: 3432,
-      date: '19 April 2022'
-    },
-    {
-      id: 2, 
-      image: Client6,
-      title: 'Psychology of intertnation Design',
-      membershipId: 3555,
-      date: '19 April 2022'
-    }
-  ])
-
-  const [knowledges] = useState([
-    {
-      id: 1,
-      name: 'Marketing',
-    },
-    {
-      id: 2,
-      name: 'Print'
-    },
-    {
-      id: 3,
-      name: 'Digital Design',
-    },
-    {
-      id: 4,
-      name: 'Social Media'
-    }, 
-    {
-      id: 5,
-      name: 'Time Management'
-    }, 
-    {
-      id: 6,
-      name: 'Communications'
-    }, 
-    {
-      id: 7,
-      name: 'Problem Solving'
-    },
-    {
-      id: 8,
-      name: 'Social Networking'
-    }, 
-    {
-      id: 9,
-      name: 'Flexibility'
+      title: 'Udacity Nanodegree Program Graduate',
+      membershipId: "AVDZL3KY",
+      date: '28 June 2022'
     }
   ])
 
   const [experiences] = useState([
     {
       id: 3, 
-      role: 'Lead UI/UX Designer',
-      text: 'Praesent dignissim sollicitudin justo, sed elementum quam lacinia quis. Phasellus eleifend tristique posuere. Sed vitae dui nec magna.',
-      company: 'Envato',
-      startYear: 2016,
+      role: 'Cloud DevOps Nanodegree',
+      text: `
+      ● Deployed a static website using AWS S3 Bucket for storing file content and AWS CloudFront for caching contents on edge location to improve latency to a lower rate by improving proximity to nearer users in specific regions.\n
+      ● Designed and deployed a web application using AWS Cloudformation , AWS S3 Bucket .
+      ● Built and automated a CI/CD pipeline using Ansible Playbook, CircleCi and AWS
+      ● Operationalized a microservice by deploying a machine learning inference API using docker and kubernetes.
+      ● Deployed a static website using AWS S3 Bucket for storing file content and AWS CloudFront for caching contents on edge location to improve latency to a lower rate by improving proximity to nearer users in specific regions. ● Designed and deployed a web application using AWS Cloudformation , AWS S3 Bucket . ● Built and automated a CI/CD pipeline using Ansible Playbook, CircleCi and AWS ● Operationalized a microservice by deploying a machine learning inference API using docker and kubernetes.
+      
+      - Skills: DevOps · Cloud Computing · AWS Command Line Interface (CLI) · Amazon EC2 · Amazon VPC · Amazon Web Services (AWS) · AWS Identity and Access Management (AWS IAM) · AWS CloudFormation · Amazon S3 · Aws CloudFront`,
+      company: 'ALX_AFRICA',
+      startYear: 2022,
       // if no finishYear => display current
     },
     {
       id: 2, 
-      role: 'Senior UI/UX Designer',
-      text: '    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor est modi voluptates enim ex aliquid tempore recusandae consequatur.',
-      company: 'Envato',
-      startYear: 2013,
-      finishYear: 2016
+      role: 'Software Engineer Intern',
+      text:`
+      Nomba (formerly Kudi), makes it easy for merchants/small businesses to digitally collect and process payments.
+      Responsibilities:
+
+      ● Implemented data storage for vendor services to ensure data persistence using Javascript.
+      ● Engaged in code base maintenance in languages such as Java , JavaScript , Python.
+      ● Uploaded POS terminal id with Java to process over 1k transactions.
+      ● Engaged in bank related document processing to prevent loss of funds.
+      ● Implementing scripts to allow fast processing of funds during fraud checks.
+      ● Maintenance of fraud checking and payment services.Nomba (formerly Kudi), makes it easy for merchants/small businesses to digitally collect and process payments. Responsibilities: ● Implemented data storage for vendor services to ensure data persistence using Javascript. ● Engaged in code base maintenance in languages such as Java , JavaScript , Python. ● Uploaded POS terminal id with Java to process over 1k transactions. ● Engaged in bank related document processing to prevent loss of funds. ● Implementing scripts to allow fast processing of funds during fraud checks. ● Maintenance of fraud checking and payment services.
+      
+      - Skills: Object-Oriented Programming (OOP) · Spring Boot · Redis · Microservices · Systems Design · JavaScript · Python (Programming Language) · Java
+      `,
+      company: 'Nomba',
+      startYear: 2022,
+      // finishYear: 2016
     },
     {
       id: 1, 
-      role: 'Junior UI/UX Designer',
-      text: 'Praesent dignissim sollicitudin justo, sed elementum quam lacinia quis. Phasellus eleifend tristique posuere. Sed vitae dui nec magna.',
-      company: 'Envato',
-      startYear: 2011,
-      finishYear: 2013
+      role: 'Software Engineer Intern',
+      text: `
+      KUDI is a financial service provider focused on providing access to electronic banking and financial services by leveraging conversational interfaces, natural language processing and artificial intelligence to provider faster access, frictionless experience, and ultimately boost financial inclusion in emerging markets. 
+
+      Responsibilities: 
+      ● Designed and Implemented a transactional feature for crediting and debiting funds using Java & Spring boot thereby eliminating the previously used company method of processing funds and improving agent satisfaction by 80% .
+      
+      ● Implemented a message resolution feature to help better inform users about transactions made . Thereby reducing consumer message issues by increasing message clarity by 70%.
+      
+      ● Fixed and maintained code base in Python , JavaScript , Java .
+      
+      ● Created jobs for slack notification .
+           
+      - Skills: Object-Oriented Programming (OOP) · Spring Boot · Redis · Systems Design · JavaScript · Python (Programming Language) · Java`,
+      company: 'Kudi',
+      startYear: 2021,
+      finishYear: 2021
     },
   ])
 
@@ -103,18 +84,20 @@ const PortfolioResume = () => {
           <div className="resume__gridLeftEducation">
             <h3>Education</h3>
             <ResumeEducation 
-              text='Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis ante.'
-              year={2008}
-              course='Frontend Development'
-              school='Columbia'
+              text='
+              ● Activities and societies: Microsoft Student Community, Google Developer Student ClubActivities and societies: Microsoft Student Community, Google Developer Student Club
+              ● Relevant Coursework: Data Structures and Analysis of Algorithms, Artificial Intelligence, Object-Oriented Programming, Introduction to Digital Systems, Computer Programming, Operating Systems Principles, Principles of Compilers.'
+              year={2016}
+              course='Computer Science And Engineering'
+              school='Obafemi Awolowo University'
               styles={{paddingBottom: '10px'}}
             />
              <ResumeEducation 
-              text='Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis ante.'
-              year={2008}
-              course='Frontend Development'
-              school='Columbia'
-              // styles={{paddingTop: '5px'}}
+              text='Udacity, Inc. is an American for-profit educational organization founded by Sebastian Thrun, David Stavens, and Mike Sokolsky offering massive open online courses'
+              year={2022}
+              course='Cloud Devops'
+              school='Udacity Nanodegree'
+              styles={{paddingTop: '5px'}}
             />
         
             <div className="resume__experience">
@@ -134,88 +117,9 @@ const PortfolioResume = () => {
             </div>
           </div>
         </div>
-        
-        <div className="resume__gridRight">
-          <h3>Design Skills</h3>
-          <ResumeProgressBar 
-            backgroundColor="#04B4E0"
-            name='Web Design'
-            percent={90}
-            border='1px solid blue'
-          />
-
-          <ResumeProgressBar 
-            backgroundColor="#04B4E0"
-            name='Print Design'
-            percent={65}
-            border='1px solid blue'
-          />
-
-          <ResumeProgressBar 
-            backgroundColor="#04B4E0"
-            name='Logo Design'
-            percent={80}
-            border='1px solid blue'
-          />
-
-          <ResumeProgressBar 
-            backgroundColor="#04B4E0"
-            name='Graphic Design'
-            percent={90}
-            border='1px solid blue'
-          />
-
-          <h3 className="resume__codingSkills">Coding Skills</h3>   
-          <ResumeProgressBar 
-            backgroundColor="#0BA376"
-            name='Javascript'
-            percent={95}
-            border='1px solid #0BA376'
-          />
-
-          <ResumeProgressBar 
-            backgroundColor="#0BA376"
-            name='PHP'
-            percent={85}
-            border='1px solid #0BA376'
-          />
-
-          <ResumeProgressBar 
-            backgroundColor="#0BA376"
-            name='HTML/CSS'
-            percent={100}
-            border='1px solid #0BA376'
-          />
-
-          <ResumeProgressBar 
-            backgroundColor="#0BA376"
-            name='Smarty/Twig'
-            percent={75}
-            border='1px solid #0BA376'
-          />
-
-          <ResumeProgressBar 
-            backgroundColor="#0BA376"
-            name='Perl'
-            percent={90}
-            border='1px solid #0BA376'
-          />      
-
-
-          <div className="resume__knowledge">
-            <h3 className="resume__knowledgeHeader">Knowledges</h3>
-
-            <div className="resume__knowledgeButtonContainer">
-              {knowledges.map((knowledge) => (
-                <ResumeKnowledgeButton styles={{ border: 'none',background: '#04B4E0', color: '#fff', padding: '10px', margin: '3px 2px', borderRadius: '5px', cursor: 'pointer' }} id={knowledge.id}  name={knowledge.name} />
-              ))}
-            </div>
-          </div> 
-
-        </div>
       </div>
 
-{/* CERTIFICATE CONTAINER */}
+      {/* CERTIFICATE CONTAINER */}
       <div className="resume__certificatesContainer">
         <h3 className="resume__certificatesHeader">Certificates</h3>
 
